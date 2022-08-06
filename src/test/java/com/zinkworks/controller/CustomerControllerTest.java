7de2fd18135a4b32150bc26b9d6da282ee53abfb
Niall Loughnane <niall1stloughnane@gmaill.com>
2022-08-06 21:30:40 +0100

    test

---
+++ src/test/java/com/zinkworks/controller/CustomerControllerTest.java
@@ -0,0 +1,15 @@
+package com.zinkworks.controller;
+
+import com.zinworks.controller.CustomerController;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+
+public class CustomerControllerTest {
+
+
+    @Test
+    public void test() {
+        CustomerController customerController = new CustomerController();
+
+    }
+}
